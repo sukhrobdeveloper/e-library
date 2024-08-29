c95bc8cffefa0f5b5e337d1f1b2376e5458f35c4
sukhrobdeveloper <sukhrobtokhirov006@gmail.com>
2024-08-29 12:51:16 +0500

    Project SetUp

---
+++ src/test/java/sukhrob/developer/e_library/ELibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package sukhrob.developer.e_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
